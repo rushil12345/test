e128f8171c12e3c158b1dde2da1e3962ba4f250d
rushil12345 <85729489+rushil12345@users.noreply.github.com>
2021-08-18 18:01:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,10 +36,10 @@ async def get_shortlink(link):
     url = 'https://shrinkme.io/api'
     params = {'api': API_KEY, 'url': link}
 
-    url = 'https://shrinkme.io/api' 
-    params = {'api': API_KEY, 'url': link}
+ 
 
-    async with aiohttp.ClientSession() as session:
+
+       async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
             data = await response.json()
             return data["shortenedUrl"]
