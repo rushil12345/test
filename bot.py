cd835292368c2436f9e999282bbff875b0b59061
rushil12345 <85729489+rushil12345@users.noreply.github.com>
2021-08-19 15:21:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,30 +31,23 @@ async def link_handler(bot, message):
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
-async def link_handler(bot, message):
-    link = message.matches[0].group(0)
-    try:
-        short_link = await get_shortlink(link)
-        await message.reply(f'Here is your {short_link}', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
-
 async def get_shortlink(link):
     url = 'https://shrinkme.io/api'
     params = {'api': API_KEY, 'url': link}
-
- 
-
-async def get_shortlink(link):
-    url = 'https://shrinkme.io/api'
-    params = {'api': API_KEY, 'url': link}
-
-
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
             data = await response.json()
             return data["shortenedUrl"]
+async def get_shortlink(link):
+    url = 'https://shrinkme.io/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
 
 
 bot.run()
