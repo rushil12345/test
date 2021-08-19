274527b3e98c3cbd77f21e14ccf488b9ca8895ef
rushil12345 <85729489+rushil12345@users.noreply.github.com>
2021-08-19 17:44:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,16 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'{short_link},  {short_link}', quote=True)
+        await message.reply(f'{short_link}', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+        
+        (filters1.regex(r'https?://[^\s]+') & filters1.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'{short_link}', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
